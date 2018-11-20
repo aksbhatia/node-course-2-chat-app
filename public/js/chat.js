b690d81deb45d899f8b0adbab8231511dd32ad63
Akshay Bhatia <emailakshaybhatia@gmail.com>
2018-11-19 19:36:14 -0800

    send only to users in my group

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,36 @@
 var socket = io()
 
 socket.on('connect', function()  {
-    console.log('connected to server')
+    var params = jQuery.deparam(window.location.search)
+
+    socket.emit('join', params, function(err) {
+        if (err) {
+            alert(err)
+            window.location.href = '/'
+        }
+        else {
+            console.log('no error')
+        }
+    })
 })
 socket.on('disconnect', function() {
     console.log('disconnected from server')
 })
 
+socket.on('updateUserList', function(users) {
+    console.log(users)
+
+    var ol = jQuery('<ol></ol>')
+
+    users.forEach(function(user) {
+        ol.append(jQuery('<li></li>').text(user))
+    })
+
+    jQuery('#users').html(ol)
+
+
+
+})
 
 socket.on('newMessage', function(message) {
 
@@ -50,7 +74,6 @@ jQuery('#message-form').on('submit', function(e) {
     var messageTextbox = jQuery('[name=message]')
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function() {
         jQuery(messageTextbox.val(''))
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
+const {Users} = require('./utils/users.js')
 
 const {generateMessage, generateLocationMessage} = require('./utils/message')
 
@@ -12,7 +13,7 @@ const port = process.env.PORT || 3001
 var app = express()
 var server = http.createServer(app)
 var io = socketIO(server)
-
+var users = new Users()
 
 app.use(express.static(publicPath))
 
@@ -22,26 +23,53 @@ io.on('connection', (socket) => {
     // socket.emit from Admin text Welcome to chat app
     // socket.brodcast.emit from Admin text new user joined
 
-    socket.emit('newMessage', generateMessage('Admin','Welcome to the Chat App!'))
+    socket.on('join', (params, callback) => {
+
+        socket.join(params.room)
+
+        users.removeUser(socket.id)
+        users.addUser(socket.id, params.name, params.room)
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room))
+        socket.emit('newMessage', generateMessage('Admin','Welcome to the Chat App!'))
         
-    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined!'))
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',`${params.name} joined!`))
+    
+        callback()
+
+    })
 
     socket.on('disconnect', () => {
-        console.log('server says: user was disconnected')
+        var user = users.removeUser(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room))
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`))
+
+        }
     })
 
     socket.on('createMessage', (message, callback) => {
-        console.log('New message:', message)
-        io.emit('newMessage', generateMessage(message.from, message.text))
+
+        var user = users.getUser(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text))
+
+        }
         callback()
 
 
     })
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+
+        var user = users.getUser(socket.id)
+
+        io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude))
     })
 })
+
 
 
 
